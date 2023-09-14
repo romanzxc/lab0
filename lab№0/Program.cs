ec11e1d919a74ed2dc9a336e605ba83e7514c800
asumibus <102917338+romanzxc@users.noreply.github.com>
2023-09-14 19:48:59 +0300

    Add files via upload

---
+++ lab№0/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_0
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Вычисления разности двух вещественных чисел
+        /// </summary>
+        /// <param name="firstNumber"> Первое вещественное число</param>
+        /// <param name="second">Второе вещественное число</param>
+        /// <returns> разность двух вещественных чисел </returns>
+        private static double DifferenceOfTwoRealNumbers(double firstNumber, double secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            double firstNumber = rnd.NextDouble();
+            double secondNumber = rnd.NextDouble();
+            Console.WriteLine("Вариант 6 : Реализовать функцию вычисления разности двух вещественных чисел ");
+            Console.WriteLine($" Первое число:{firstNumber} ; Второе число:{secondNumber} ; \n  Разность вещественных чисел: {DifferenceOfTwoRealNumbers(firstNumber, secondNumber)}");
+            Console.ReadLine();
+        }
+    }
+}
